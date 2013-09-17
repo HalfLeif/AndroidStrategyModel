f3f0a99a5e798783f2953c2d6e7d838ea4043a3d
Leif Schelin <edarathaan@hotmail.com>
2013-09-17 07:00:26 +0200

    new enum Terrain

---
+++ AndroidStrategy/src/com/group8/AndroidFantasy/model/grid/Terrain.java
@@ -0,0 +1,18 @@
+package com.group8.AndroidFantasy.model.grid;
+
+public enum Terrain {
+	GRASS(1),
+	FOREST(2),
+	HILL(2);
+	
+	private final int moveCost;
+	
+	private Terrain(int moveCost){
+		this.moveCost = moveCost;
+	}
+	
+	public int getMoveCost(){
+		return moveCost;
+	}
+
+}
