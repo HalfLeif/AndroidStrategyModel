f1883eaa0f28ba783c7da71116c6ca756a3b07c2
Leif Schelin <edarathaan@hotmail.com>
2013-09-17 07:02:40 +0200

    Space holds a Terrain

--- AndroidStrategy/src/com/group8/AndroidFantasy/model/grid/Space.java
+++ AndroidStrategy/src/com/group8/AndroidFantasy/model/grid/Space.java
@@ -1,5 +1,12 @@
 package com.group8.AndroidFantasy.model.grid;
 
 public class Space {
-
+	private final Terrain terr;
+	
+	public Space(Terrain terr){
+		this.terr=terr;
+	}
+	public Space(){
+		this(Terrain.GRASS);
+	}
 }
